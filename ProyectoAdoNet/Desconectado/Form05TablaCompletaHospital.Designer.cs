27bfca5e96f0a6c60af48e43d653aa5113f9375a
jgiganto <jgiganto1@hotmail.com>
2018-01-12 10:52:34 +0100

    a

--- ProyectoAdoNet/Desconectado/Form05TablaCompletaHospital.Designer.cs
+++ ProyectoAdoNet/Desconectado/Form05TablaCompletaHospital.Designer.cs
@@ -28,21 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lsvhospitales = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lsvhospitales
+            // 
+            this.lsvhospitales.Location = new System.Drawing.Point(25, 75);
+            this.lsvhospitales.Name = "lsvhospitales";
+            this.lsvhospitales.Size = new System.Drawing.Size(662, 362);
+            this.lsvhospitales.TabIndex = 0;
+            this.lsvhospitales.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hospitales";
             // 
             // Form05TablaCompletaHospital
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(737, 525);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lsvhospitales);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form05TablaCompletaHospital";
             this.Text = "Form05TablaCompletaHospital";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lsvhospitales;
+        private System.Windows.Forms.Label label1;
     }
 }
